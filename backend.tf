bef048f6500f23897e1375a36c1331d1bf82802b
Har0709 <128058009+Har0709@users.noreply.github.com>
2026-07-31 18:46:46 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-/* terraform {
-  backend "s3" {
-    bucket         = "my-ews-buket1"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    use_lockfile   = true
-    encrypt        = true
-  }
-}  */
