21065bd0ec7cce98271c57a1f1ff17011b0c33a3
Har0709 <128058009+Har0709@users.noreply.github.com>
2026-07-31 19:34:53 +0530

    refactor1

--- eks/variables.tf
+++ eks/variables.tf
@@ -1,51 +1,81 @@
-variable "aws-region" {}
-variable "env" {}
-variable "cluster-name" {}
-variable "vpc-cidr-block" {}
-variable "vpc-name" {}
-variable "igw-name" {}
-variable "pub-subnet-count" {}
-variable "pub-cidr-block" {
-  type = list(string)
-}
-variable "pub-availability-zone" {
-  type = list(string)
-}
-variable "pub-sub-name" {}
-variable "pri-subnet-count" {}
-variable "pri-cidr-block" {
-  type = list(string)
-}
-variable "pri-availability-zone" {
-  type = list(string)
-}
-variable "pri-sub-name" {}
-variable "public-rt-name" {}
-variable "private-rt-name" {}
-variable "eip-name" {}
-variable "ngw-name" {}
-variable "eks-sg" {}
+##########################################################
+# AWS
+##########################################################
 
+variable "region" {
 
+  default = "ap-south-1"
+}
+
+##########################################################
+# PROJECT
+##########################################################
+
+variable "project_name" {
+
+  default = "eks-demo"
+}
+
+variable "environment" {
+
+  default = "dev"
+}
+
+##########################################################
+# NETWORK
+##########################################################
+
+variable "vpc_cidr" {
+
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+
+  type = list(string)
+
+  default = [
+
+    "10.0.1.0/24",
+
+    "10.0.2.0/24"
+  ]
+}
+
+variable "private_subnets" {
+
+  type = list(string)
+
+  default = [
+
+    "10.0.11.0/24",
+
+    "10.0.12.0/24"
+  ]
+}
+
+variable "availability_zones" {
+
+  type = list(string)
+
+  default = [
+
+    "ap-south-1a",
+
+    "ap-south-1b"
+  ]
+}
+
+##########################################################
 # EKS
-variable "is-eks-cluster-enabled" {}
-variable "cluster-version" {}
-variable "endpoint-private-access" {}
-variable "endpoint-public-access" {}
-variable "ondemand_instance_types" {
-  default = ["t3a.medium"]
+##########################################################
+
+variable "cluster_name" {
+
+  default = "eks-demo"
 }
 
-variable "spot_instance_types" {}
-variable "desired_capacity_on_demand" {}
-variable "min_capacity_on_demand" {}
-variable "max_capacity_on_demand" {}
-variable "desired_capacity_spot" {}
-variable "min_capacity_spot" {}
-variable "max_capacity_spot" {}
-variable "addons" {
-  type = list(object({
-    name    = string
-    version = string
-  }))
+variable "cluster_version" {
+
+  default = "1.33"
 }
