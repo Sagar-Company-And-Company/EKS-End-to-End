88120ebb5a652e36af41e831c19df97bb9dc788a
Har0709 <128058009+Har0709@users.noreply.github.com>
2026-07-31 19:29:07 +0530

    Refactor IAM roles and policies for EKS setup

--- module/iam.tf
+++ module/iam.tf
@@ -1,97 +1,138 @@
-locals {
-  cluster_name = var.cluster-name
+##########################################################
+# VARIABLES
+##########################################################
+
+variable "project_name" {
+  default = "eks-demo"
 }
 
-resource "random_integer" "random_suffix" {
-  min = 1000
-  max = 9999
-}
+##########################################################
+# EKS CLUSTER IAM ROLE
+##########################################################
 
-resource "aws_iam_role" "eks-cluster-role" {
-  count = var.is_eks_role_enabled ? 1 : 0
-  name  = "${local.cluster_name}-role-${random_integer.random_suffix.result}"
+resource "aws_iam_role" "eks_cluster_role" {
+
+  name = "${var.project_name}-cluster-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
       }
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
+
+  tags = {
+    Name = "${var.project_name}-cluster-role"
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  count      = var.is_eks_role_enabled ? 1 : 0
+##########################################################
+# ATTACH CLUSTER POLICIES
+##########################################################
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+
+  role = aws_iam_role.eks_cluster_role.name
+
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-cluster-role[count.index].name
 }
 
-resource "aws_iam_role" "eks-nodegroup-role" {
-  count = var.is_eks_nodegroup_role_enabled ? 1 : 0
-  name  = "${local.cluster_name}-nodegroup-role-${random_integer.random_suffix.result}"
+resource "aws_iam_role_policy_attachment" "vpc_controller" {
+
+  role = aws_iam_role.eks_cluster_role.name
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+##########################################################
+# NODE GROUP IAM ROLE
+##########################################################
+
+resource "aws_iam_role" "node_group_role" {
+
+  name = "${var.project_name}-node-role"
 
   assume_role_policy = jsonencode({
+
     Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
+
+    Statement = [
+
+      {
+
+        Effect = "Allow"
+
+        Principal = {
+
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
       }
-    }]
+    ]
   })
+
+  tags = {
+    Name = "${var.project_name}-node-role"
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "eks-AmazonWorkerNodePolicy" {
-  count      = var.is_eks_nodegroup_role_enabled ? 1 : 0
+##########################################################
+# NODE GROUP POLICIES
+##########################################################
+
+resource "aws_iam_role_policy_attachment" "worker_node" {
+
+  role = aws_iam_role.node_group_role.name
+
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks-nodegroup-role[count.index].name
 }
 
-resource "aws_iam_role_policy_attachment" "eks-AmazonEKS_CNI_Policy" {
-  count      = var.is_eks_nodegroup_role_enabled ? 1 : 0
+resource "aws_iam_role_policy_attachment" "cni" {
+
+  role = aws_iam_role.node_group_role.name
+
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks-nodegroup-role[count.index].name
 }
-resource "aws_iam_role_policy_attachment" "eks-AmazonEC2ContainerRegistryReadOnly" {
-  count      = var.is_eks_nodegroup_role_enabled ? 1 : 0
+
+resource "aws_iam_role_policy_attachment" "ecr" {
+
+  role = aws_iam_role.node_group_role.name
+
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks-nodegroup-role[count.index].name
 }
 
-resource "aws_iam_role_policy_attachment" "eks-AmazonEBSCSIDriverPolicy" {
-  count      = var.is_eks_nodegroup_role_enabled ? 1 : 0
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-  role       = aws_iam_role.eks-nodegroup-role[count.index].name
+##########################################################
+# OPTIONAL
+# SSM ACCESS TO EC2 NODES
+##########################################################
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+
+  role = aws_iam_role.node_group_role.name
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# OIDC
-resource "aws_iam_role" "eks_oidc" {
-  assume_role_policy = data.aws_iam_policy_document.eks_oidc_assume_role_policy.json
-  name               = "eks-oidc"
+##########################################################
+# OUTPUTS
+##########################################################
+
+output "cluster_role_arn" {
+
+  value = aws_iam_role.eks_cluster_role.arn
 }
 
-resource "aws_iam_policy" "eks-oidc-policy" {
-  name = "test-policy"
+output "node_role_arn" {
 
-  policy = jsonencode({
-    Statement = [{
-      Action = [
-        "s3:ListAllMyBuckets",
-        "s3:GetBucketLocation",
-        "*"
-      ]
-      Effect   = "Allow"
-      Resource = "*"
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks-oidc-policy-attach" {
-  role       = aws_iam_role.eks_oidc.name
-  policy_arn = aws_iam_policy.eks-oidc-policy.arn
+  value = aws_iam_role.node_group_role.arn
 }
